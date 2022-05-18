b1cfc496b9cee9f29ef5cc41120b2a7e93e8d10c
crforsythe <cforsyth@andrew.cmu.edu>
2022-05-18 10:52:50 -0400

    Initial Commit

---
+++ GenerateCBCDesign.R
@@ -0,0 +1,21 @@
+library(conjointTools)
+
+set.seed(1995912)
+
+levelValues <- list(
+  price = c("$0", "$5", "$10", "$15", "$20", "$25", "$30"),
+  time = c("Today", 'Tomorrow', 'In 4 days', 'In 7 days'),
+  agent = c('Human', 'Autonomous vehicle'),
+  location = c('Doorstep', 'Curbside', 'Neighborhood locker')
+)
+
+doe <- makeDoe(levelValues)
+doe <- recodeDoe(doe, levelValues)
+
+numberOfRespondents <- 750
+numberOfQuestions <- 10
+numberOfAlts <- 2
+outsideGood <- FALSE
+
+surveyValues <- makeSurvey(doe, nResp = numberOfRespondents, nQPerResp = numberOfQuestions, nAltsPerQ = numberOfAlts, outsideGood = outsideGood)
+write.csv(surveyValues, 'Designs/CMUGrocery0.csv')
